aba196c500087616f3980f00175e67c2829363b7
victorloo <ila9970@gmail.com>
2018-03-29 22:54:11 +0800

    complete Q3 25% off

--- lib/cart.rb
+++ lib/cart.rb
@@ -10,7 +10,9 @@ class Cart
     total = 0
     
     while @ordered_items.size > 0
-      if @ordered_items.size == 4
+      if @ordered_items.size == 5
+        total = total + 5 * PRICE * 0.75
+      elsif @ordered_items.size == 4
         total = total + 4 *PRICE * 0.8
       elsif @ordered_items.size == 3
         total = total + 3 * PRICE * 0.9
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -107,4 +107,43 @@ describe Cart do
 
   end
 
+  context "第五種情境︰五本不同集的書打 25% 折扣" do
+
+    it "第一集買一本、第二集買一本、第三集買一本、第四集買一本、第五集買一本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 1, "5th": 1 })
+      expect(@cart.calculate).to eq(375)
+    end
+
+    it "第一集買兩本、第二集買兩本、第三集買兩本、第四集買兩本、第五集買兩本" do
+      @cart.add({ "1st": 2, "2nd": 2, "3rd": 2, "4th": 2, "5th": 2 })
+      expect(@cart.calculate).to eq(750)
+    end
+
+    it "第一集買一本、第二集買兩本、第三集買一本、第四集買兩本、第五集買一本" do
+      @cart.add({ "1st": 1, "2nd": 2, "3rd": 1, "4th": 2, "5th": 1 })
+      expect(@cart.calculate).to eq(565)
+    end
+
+    it "第一集買一本、第二集買兩本、第三集買三本、第四集買四本、第五集買五本" do
+      @cart.add({ "1st": 1, "2nd": 2, "3rd": 3, "4th": 4, "5th": 5 })
+      expect(@cart.calculate).to eq(1255)
+    end
+
+    it "第一集買兩本、第二集買三本、第三集買四本、第四集買五本、第五集買六本" do
+      @cart.add({ "1st": 2, "2nd": 3, "3rd": 4, "4th": 5, "5th": 6 })
+      expect(@cart.calculate).to eq(1630)
+    end
+
+    it "第一集買兩本、第二集買三本、第三集買一本、第四集買四本、第五集買三本" do
+      @cart.add({ "1st": 2, "2nd": 3, "3rd": 1, "4th": 4, "5th": 3 })
+      expect(@cart.calculate).to eq(1065)
+    end
+
+    it "第一集買三本、第二集買一本、第三集買一本、第四集買四本、第五集買六本" do
+      @cart.add({ "1st": 3, "2nd": 1, "3rd": 1, "4th": 4, "5th": 6 })
+      expect(@cart.calculate).to eq(1305)
+    end
+
+  end
+
 end
